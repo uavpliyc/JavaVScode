85c15efb0c3ca0b09c7ef92551f36a09af9e3fc6
uavpliyc <uavpliyc@gmail.com>
2021-09-25 09:40:53 +0900

    fix demo

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,5 +16,11 @@ public class DemoController {
   public void method(@RequestParam("name") String name, Model model) {
     model.addAttribute("name", "Hello Thymeleaf");
     model.addAttribute("text", name);
+    List<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    list.add("C");
+    model.addAttribute("list", list);
+
   }
 }
