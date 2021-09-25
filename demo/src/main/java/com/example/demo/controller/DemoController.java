b3fe6d729d70a046ac25a7b9d7e1fa5ae731d151
uavpliyc <uavpliyc@gmail.com>
2021-09-26 08:16:15 +0900

    add err

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,22 +1,24 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import lombok.NonNull;
-
 @Controller
 public class DemoController {
 
   @RequestMapping(value = "/demo", method = { RequestMethod.POST, RequestMethod.GET })
-  public ModelAndView method(@RequestParam("param1") String param1,@RequestParam("param2") String param2,@RequestParam("symbol") String symbol, ModelAndView mav, form form) {
+  public ModelAndView method(@RequestParam("param1") String param1, @RequestParam("param2") String param2,
+      @RequestParam("symbol") String symbol, ModelAndView mav, @Validated form form, BindingResult result) {
+
+    if (result.hasErrors()) {
+    }
 
     // セッション情報登録
     mav.addObject("param1", param1);
@@ -30,23 +32,35 @@ public class DemoController {
 }
 
 class form {
-  @NonNull
+  @NotNull
   private int param1;
-  @NonNull
+  @NotNull
   private int param2;
-  @NonNull
+  @NotNull
   private String symbol;
 
-  // getter
+  // getter.setter
   public int getParam1() {
     return param1;
+  }
+
+  public void setParam1(int param1) {
+    this.param1 = param1;
   }
 
   public int getParam2() {
     return param2;
   }
 
+  public void setParam2(int param2) {
+    this.param1 = param2;
+  }
+
   public String getSymbol() {
     return symbol;
+  }
+
+  public void setSymbol(String symbol) {
+    this.symbol = symbol;
   }
 }
